e28c31ee47c31f20363176481565f8058f6b68e3
oiizes <oiizesgitlab@gmail.com>
2019-10-25 16:16:59 +0300

    Creating new Footer, more appealing

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -5,46 +5,46 @@ const Footer = () => {
     return (
       <div>
 
-<footer class="page-footer font-small special-color-dark pt-4">
+<footer>
+<div class="mt-5 pt-5 pb-5 footer">
 
-  <div class="container">
-
-    <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-          <i class="fab fa-facebook-f"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-          <i class="fab fa-twitter"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-          <i class="fab fa-google-plus-g"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
-          <i class="fab fa-linkedin-in"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-dribbble mx-1">
-          <i class="fab fa-dribbble"> </i>
-        </a>
-      </li>
-    </ul>
-
+  <div class="row">
+    <div class="col-lg-6 col-xs-12 about-company">
+      <h4>Love where you’re going..</h4>
+      <p>Welcome to Ethiopia- an ancient wonderland with rich cultural traditions, architectural remains, natural beauty and enormously hospitable people. Ethiopia has a history of over 3,000 years, which has been preserved in a remarkable manner. Only in Ethiopia will you find yourself dramatically transported back in time through the participation in sacred rituals of an archaic faith.
+      While we cannot guarantee the whole 7 years or any permanent effects, traveling with Travel Ethiopia and the combination of our special knowledge and Ethiopia’s range of human and natural diversity will be an enriching experience.
+      </p>
+      <p><a href="facebook.com"><i class="fab fa-facebook mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+    </div>
+    <div class="col-lg-3 col-xs-12 links">
+      <h4 class="mt-lg-0 mt-sm-3">Let's Connect</h4>
+        <ul class="m-0 p-0 links">
+          <li class="link"><a href="#">Lorem ipsum</a></li>
+          <li class="link"><a href="#">Nam mauris velit</a></li>
+          <li class="link"><a href="#">Etiam vitae mauris</a></li>
+          <li class="link"><a href="#">Fusce scelerisque</a></li>
+          <li class="link"><a href="#">Sed faucibus</a></li>
+          <li class="link"><a href="#">Mauris efficitur nulla</a></li>
+        </ul>
+    </div>
+    <div class="col-lg-3 col-xs-12 location">
+      <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+      <p>Ayat Apartment ground and first Floor
+              Walking Distance from UNECA, Guinea Conakry Street, 
+              Addis Ababa  Ethiopia </p>
+      <p class="mb-0">Office:   +251 115 52 54 78</p>
+      <p>Email:  info@travelethiopia.com</p>
+      <p>Po.Box. 9438</p>
+    </div>
   </div>
-  <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  <div class="row mt-5">
+    <div class="col copyright">
+      <p class=""><small class="text-white-50">© 2019. All Rights Reserved.</small></p>
+    </div>
   </div>
+</div>
 
 </footer>
-
-
 
 </div>
     );
